d886b6dafa0565ea3ca874a01be02cea764b1865
Tony Hesjevik <tony@hesjevik.no>
2015-03-20 12:42:27 +0100

    Rename deploy script to ship

--- scripts/ship.sh
+++ scripts/ship.sh
@@ -8,14 +8,14 @@ cd $(dirname "${BASH_SOURCE[0]}") && cd ../
 
 declare IMAGE_NAME="bachelorthesis/archlinux"
 
-deploy() {
+ship() {
 	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
 	docker push "$IMAGE_NAME"
 }
 
 if [[ -z $(which docker) ]]; then
-    echo "Missing docker client which is required for deployment."
+    echo "Missing docker client which is required for shipping."
     exit 2
 fi
 
-deploy
+ship
