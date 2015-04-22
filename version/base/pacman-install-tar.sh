a158ec885e4aa2ca5a46cc5e0db2dfcfb444eebb
Tony Hesjevik <tony@hesjevik.no>
2015-04-23 00:25:18 +0200

    Make pacman clear cache when not sudo

--- version/base/pacman-install-tar.sh
+++ version/base/pacman-install-tar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $EUID -ne 0 ]]; then
-    sudo pacman -U --noconfirm "$@" && rm -Rf /var/cache/pacman/pkg/*
+    sudo pacman -U --noconfirm "$@" && sudo rm -Rf /var/cache/pacman/pkg/*
 else
     pacman -U --noconfirm "$@" && rm -Rf /var/cache/pacman/pkg/*
 fi
