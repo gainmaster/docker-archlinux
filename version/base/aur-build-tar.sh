7ae8cb7df97cb02ac731ca27881fc9742ab41417
Tony Hesjevik <tony@hesjevik.no>
2015-04-23 17:01:37 +0200

    Fix aur build tar

--- version/base/aur-build-tar.sh
+++ version/base/aur-build-tar.sh
@@ -2,18 +2,18 @@
 
 SCRIPT_DIRECTORY=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
+export AUR_BUILD_DIRECTORY=${AUR_BUILD_DIRECTORY:-$PWD}
+
 if [[ $EUID -eq 0 ]]; then
-	runuser -l admin -c "$SCRIPT_DIRECTORY/aur-build-tar $@"
+	runuser -l admin -c "AUR_BUILD_DIRECTORY=$AUR_BUILD_DIRECTORY $SCRIPT_DIRECTORY/aur-build-tar $@"
     exit 0
 fi
 
-declare BUILD_DIRECTORY=${AUR_BUILD_DIRECTORY:-$PWD}
-
 for package in ${@}; do
-    cd $BUILD_DIRECTORY
+    cd $AUR_BUILD_DIRECTORY
     curl https://aur.archlinux.org/packages/${package:0:2}/$package/$package.tar.gz | tar xz
     cd $package
-    makepkg $package 
-    mv "${package}*.pkg.tar.xz" "${package}.pkg.tar.xz"
-    cd $BUILD_DIRECTORY && rm -Rf $package
+    makepkg $package --skippgpcheck
+    mv ${package}*.pkg.tar.xz ${package}.pkg.tar.xz
+    cd $AUR_BUILD_DIRECTORY && rm -Rf $package
 done
